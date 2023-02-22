a859074c4712e0668512927b4f41c5e6fe4b5ac9
AmirDanesh <daneshahd78@gmail.com>
2023-02-22 15:23:12 +0330

    Get Api from real Backend

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,37 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Menu = ({ allMenus }) => {
-
+  const [allProducts , setAllProducts] = useState([])
   useEffect(() => {
-    let prices = document.querySelectorAll(".price");
-    prices.forEach((price) => {
-      price.innerHTML = price.innerHTML.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    })
+    // let prices = document.querySelectorAll(".price");
+    // prices.forEach((price) => {
+    //   price.innerHTML = price.innerHTML.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    // })
+    fetch("https://mdresturant.iran.liara.run/api/products/")
+    .then(res=> res.json())
+    .then(data=> setAllProducts(data))
+
   })
 
 
 
   return (
     <div className="section-center">
-      {allMenus.map(menu => {
+      {allProducts.map(pr => {
         return (
-          <article key={menu.id} className="menu-item">
-            <img src={menu.img}
+          <article key={pr.id} className="menu-item">
+            <img src={pr.img}
               className="photo"
-              alt={menu.desc}
+              alt={pr.desc}
             />
             <div className="item-info">
               <header>
-                <h4>{menu.title}</h4>
+                <h4>{pr.title}</h4>
 
                 <h4 className="price">
-                  {menu.price}  
+                  {pr.price}  
                   <span className="toman">
                       تومان  
                   </span>
                 </h4>
               </header>
-              <p className="item-text">{menu.desc}</p>
+              <p className="item-text">{pr.desc}</p>
             </div>
           </article>
         )
